0d49812af322a60b2a4946cf04ab3ce54f2c9c62
Daniel <danielvillasanti2132@gmail.com>
2023-10-02 23:18:31 -0300

    style: el sitio es responsive

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,21 @@ module.exports = {
 				'./node_modules/flowbite/**/*.js'],
 	theme: {
 		extend: {},
+		screens: {
+			"2xl": { max: "1535px" },
+			xl: { max: "1279px" },
+			bxl: { max: "1170px" },
+			lGG: { max: "1040px" },
+			lg: { max: "1023px" },
+			blg: { max: "910px" },
+			md: { max: "880px" },
+			sm: { max: "680px" },
+			bsm: { max: "580px" },
+			xr: { max: "420px" },
+			se: { max: "375px" },
+			mn: { min: "767px" },
+			blgMin: { min: "910px" },
+		  },
 	},
 	plugins: [
 		require("daisyui"),
